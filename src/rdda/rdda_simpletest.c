c9565b02b897c81f4eea4cb106cd355c72d0ebf4
Stephenyu113 <stephenyu113@outlook.com>
2019-04-25 17:30:15 -0400

    debug

--- src/rdda/rdda_simpletest.c
+++ src/rdda/rdda_simpletest.c
@@ -22,8 +22,8 @@ void rdda_simpletest(void *ifnameptr)
     char *ifname = ifnameptr;
     SlaveIndex *slaveIndex;
     int motor[2], psensor;
-//    double theta_rad[2];
-//    double comp_Nm[2];
+    double theta_rad[2];
+    double comp_Nm[2];
 
 //    int64 NSEC_PER_SEC = 1000000000;
     double COUNTS_PER_RADIAN = 52151.8917;
@@ -58,18 +58,12 @@ void rdda_simpletest(void *ifnameptr)
     {
         ec_receive_processdata(EC_TIMEOUTRET);
 
-//        theta_rad[0] = (double)(motorIn[motor[0]]->act_pos)/COUNTS_PER_RADIAN;
-//        theta_rad[1] = (double)(motorIn[motor[1]]->act_pos)/COUNTS_PER_RADIAN;
-//        comp_Nm[0] = (double)(pressureIn->val1) * PASCAL_PER_COUNT * NM_PER_PASCAL;
-//        comp_Nm[1] = (double)(pressureIn->val2) * PASCAL_PER_COUNT * NM_PER_PASCAL;
-
-//        printf("theta1: %+2.4lf, theta2: %+2.4lf, pressure1: %+2.4f, pressure2: %+2.4lf\r", theta_rad[0], theta_rad[1], comp_Nm[0], comp_Nm[1]);
-
-        printf("theta1: %+2.4lf, theta2: %+2.4lf, pressure1: %+2.4f, pressure2: %+2.4lf\r",
-                (double)(motorIn[0]->act_pos)/COUNTS_PER_RADIAN,
-                (double)(motorIn[1]->act_pos)/COUNTS_PER_RADIAN,
-                (double)(pressureIn->val1) * PASCAL_PER_COUNT * NM_PER_PASCAL,
-                (double)(pressureIn->val2) * PASCAL_PER_COUNT * NM_PER_PASCAL);
+        theta_rad[0] = (double)(motorIn[0]->act_pos)/COUNTS_PER_RADIAN;
+        theta_rad[1] = (double)(motorIn[1]->act_pos)/COUNTS_PER_RADIAN;
+        comp_Nm[0] = (double)(pressureIn->val1) * PASCAL_PER_COUNT * NM_PER_PASCAL;
+        comp_Nm[1] = (double)(pressureIn->val2) * PASCAL_PER_COUNT * NM_PER_PASCAL;
+        
+        printf("theta1: %+2.4lf, theta2: %+2.4lf, pressure1: %+2.4f, pressure2: %+2.4lf\r", theta_rad[0], theta_rad[1], comp_Nm[0], comp_Nm[1]);
         fflush(stdout);
 
         ec_send_processdata();
