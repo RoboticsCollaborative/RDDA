0e7a96f3b507b326bee6f7b6cd10c17c73fb7408
Chunpeng19 <chunpengwang1989@gmail.com>
2021-01-14 17:45:18 -0500

    force feedback with DOB test

--- src/rdda/rdda_base.c
+++ src/rdda/rdda_base.c
@@ -51,7 +51,7 @@ void rdda_update(ecat_slaves *ecatSlaves, Rdda *rdda) {
 
     /* Outputs */
     ecatSlaves->bel[0].out_motor->ctrl_wd = 15;//15;
-    ecatSlaves->bel[1].out_motor->ctrl_wd = 15;
+    ecatSlaves->bel[1].out_motor->ctrl_wd = 0;
     //ecatSlaves->bel[2].out_motor->ctrl_wd = 15;
     for (int j = 0; j < 2; j++) {
         //ecatSlaves->bel[j].out_motor->ctrl_wd = 0;
