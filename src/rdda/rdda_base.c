684ecc86730b7177c16902cb6af41d1a4495706f
Chunpeng19 <chunpengwang1989@gmail.com>
2021-01-27 18:52:11 -0500

    tele-op driver test

--- src/rdda/rdda_base.c
+++ src/rdda/rdda_base.c
@@ -50,10 +50,10 @@ void rdda_update(ecat_slaves *ecatSlaves, Rdda *rdda) {
     rdda->ts.sec = ecatSlaves->ts.tv_sec;
 
     /* Outputs */
-    ecatSlaves->bel[0].out_motor->ctrl_wd = 15;//15;
-    ecatSlaves->bel[1].out_motor->ctrl_wd = 15;
-    ecatSlaves->bel[2].out_motor->ctrl_wd = 15;
-    ecatSlaves->bel[3].out_motor->ctrl_wd = 15;
+    ecatSlaves->bel[0].out_motor->ctrl_wd = 0;//15;
+    ecatSlaves->bel[1].out_motor->ctrl_wd = 0;
+    ecatSlaves->bel[2].out_motor->ctrl_wd = 0;
+    ecatSlaves->bel[3].out_motor->ctrl_wd = 0;
     for (int j = 0; j < 4; j++) {
         //ecatSlaves->bel[j].out_motor->ctrl_wd = 0;
         ecatSlaves->bel[j].out_motor->tg_pos = (int32)saturation(limit_int32, ecatSlaves->bel[j].init_pos_cnts + (int32)saturation(limit_int32, rdda->motor[j].motorOut.tg_pos * ecatSlaves->bel[j].counts_per_rad));
